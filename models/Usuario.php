13b57d9a7fcf396b514d21855ea23cc1fb4e21ca
Edson Rodrigues <edsonrb.2008@gmail.com>
2023-08-31 19:22:02 -0300

    Inclusão dos Try Catch

--- models/Usuario.php
+++ models/Usuario.php
@@ -12,55 +12,80 @@ class Usuario
 
     }
 
+    // Função para listar todos os usuarios da tabela
     public function listar()
     {
-        $query = "SELECT * FROM {$this->table}";
-        $stmt = $this->db->query($query);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        try {
+            $query = "SELECT * FROM {$this->table}";
+            $stmt = $this->db->query($query);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
     }
 
+    // Função para buscar um usuario pelo seu ID
     public function buscar($id)
     {
-        $query = "SELECT * FROM {$this->table} WHERE id = :id";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        try {
+            $query = "SELECT * FROM {$this->table} WHERE id = :id";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
     }
 
+    // Função para cadastrar um novo registro na tabela usuário
     public function cadastrar($data)
     {
-        $query = "INSERT INTO {$this->table} (nome, email, senha, perfil) VALUES (:nome, :email, :senha, :perfil)";
-        $stmt = $this->db->prepare($query);
+        try {
+            $query = "INSERT INTO {$this->table} (nome, email, senha, perfil) VALUES (:nome, :email, :senha, :perfil)";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindParam(':nome', $data['nome']);
+            $stmt->bindParam(':email', $data['email']);
+            $stmt->bindParam(':senha', $data['senha']);
+            $stmt->bindParam(':perfil', $data['perfil']);
 
-        $stmt->bindParam(':nome', $data['nome']);
-        $stmt->bindParam(':email', $data['email']);
-        $stmt->bindParam(':senha', $data['senha']);
-        $stmt->bindParam(':perfil', $data['perfil']);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
 
-        $stmt->execute();
     }
 
+    // Função para editar um registro existente na tabela usuário
     public function editar($id, $data)
     {
-        $query = "UPDATE {$this->table} SET nome = :nome, email = :email, senha = :senha, perfil = :perfil WHERE id = :id";
-        $stmt = $this->db->prepare($query);
-
-        $stmt->bindParam(':nome', $data['nome']);
-        $stmt->bindParam(':email', $data['email']);
-        $stmt->bindParam(':senha', $data['senha']);
-        $stmt->bindParam(':perfil', $data['perfil']);
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-
-        $stmt->execute();
+        try {
+            $query = "UPDATE {$this->table} SET nome = :nome, email = :email, senha = :senha, perfil = :perfil WHERE id = :id";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindParam(':nome', $data['nome']);
+            $stmt->bindParam(':email', $data['email']);
+            $stmt->bindParam(':senha', $data['senha']);
+            $stmt->bindParam(':perfil', $data['perfil']);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
 
+    // Função para excluir um registro da tabela usuario pelo seu ID
     public function excluir($id)
     {
-        $query = "DELETE FROM {$this->table} WHERE id = :id";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->execute();
+        try {
+            $query = "DELETE FROM {$this->table} WHERE id = :id";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
 
 }
